a7f713a3af4304f7cc1b4369459885ae4a964337
Justin Frasier <Justin_Frasier@me.com>
2017-04-25 23:17:04 -0500

    Main Page and to init the site

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var	sass = require('gulp-sass');
+var	browserSync = require('browser-sync');
+var jade = require('gulp-jade');
+var	paths = {scss:'./sass/*.sass'};
+
+gulp.task('sass', function (){
+	gulp.src('./sass/*.sass')
+		.pipe(sass({outputStyle:'compressed'}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('jade' , function(){
+	gulp.src('./jade/index/index.jade')
+		.pipe(jade({
+			pretty: false
+			}))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task("jadeUpdated", function(){
+	gulp.src('./jade/updated/index.jade')
+		.pipe(jade({
+			pretty: false
+		}))
+		.pipe(gulp.dest('./dist'))
+});
+
+gulp.task('brower-sync', function(){
+	browserSync.init(['./css/*.css', './js/*.js', './*.html'],{ server:{baseDir:'./'}});
+});
+
+
+
+gulp.task('watch', function(){
+	gulp.watch(['sass/*.sass', 'sass/*/*.sass'],['sass']);
+	gulp.watch('./jade/*/*.jade',['jade']);
+});
+
+gulp.task('default',['jade','jadeUpdated', 'sass', 'brower-sync', 'watch']);
