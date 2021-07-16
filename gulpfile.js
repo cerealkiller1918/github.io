e57d879380c803b1163f40537fc4a3b2cddd7a0a
Justin Frasier <justinfrasier@killermediagroup.com>
2021-07-15 22:35:51 -0500

    updated gulp to new gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,58 @@
-var gulp = require('gulp');
-var	sass = require('gulp-sass');
+var {src, dest, watch, series} =  require('gulp');
+var sass = require('gulp-sass')(require('sass'));
 var	browserSync = require('browser-sync');
-var pug = require('gulp-pug');
-var	paths = {scss:'./sass/*.sass'};
+var jade = require('gulp-jade');
 
-gulp.task('sass', function (){
-	gulp.src('./sass/*.sass')
-		.pipe(sass())
-		.pipe(gulp.dest('css'));
-});
+var log = (done) => {
+    console.log('Hello Gulp!');
+    done();
+}
 
-gulp.task('pug' , function(){
-	gulp.src('./pug/index/index.pug')
-		.pipe(pug())
-		.pipe(gulp.dest('./'));
-});
+var html = (done) => {
+    src('./jade/index/index.jade')
+		.pipe(jade({pretty: false}))
+		.pipe(dest('dist'));
+    done();
+}
+
+var css = (done) => {
+	src('./sass/*.sass')
+		.pipe(sass({outputStyle:'compressed'}))
+		.pipe(dest('dist/css'));
+	done();
+}
+
+var move = (done) => {
+    src('.files/fonts/*')
+        .pipe(dest("dist/fonts"));
+    src('.files/flaticon/*')
+        .pipe(dest("dist/flaticon"));
+    src('.files/css/*')
+        .pipe(dest("dist/css"));
+    src('.files/js/*')
+        .pipe(dest("dist/js"));
+    src('.files/photo/*')
+        .pipe(dest("dist/photo"));
+    
+    done();
+}
+
+async function browser() {
+    browserSync.init(['./css/*.css', './js/*.js', './*.html'],{ server:{baseDir:'./'}});
+}
+
+async function update() {
+    watch(['sass/*.sass', 'sass/*/*.sass'], css);
+    watch('./jade/index/*/*.jade', html);
+}
 
 
-gulp.task('brower-sync', function(){
-	browserSync.init(['./css/*.css', './js/*.js', './*.html'],{ server:{baseDir:'./'}});
-});
 
 
-
-gulp.task('watch', function(){
-	gulp.watch(['sass/*.sass', 'sass/*/*.sass'],['sass']);
-	gulp.watch('./pug/*/*.pug',['pug']);
-});
-
-gulp.task('default',['pug', 'sass', 'brower-sync', 'watch']);
+exports.log = log;
+exports.html = html;
+exports.css = css;
+exports.browser = browser;
+exports.update = update;
+exports.move = move;
+exports.default = series(html, css, browser, update);
